122ea59437cf969e987674ffdfb8c42921a80f3c
edurso <eric@casadurso.com>
2022-02-08 16:07:50 -0500

    initial commit

---
+++ data.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+data_frame = pd.read_csv('election-data-congressional-district.csv')
+print(data_frame)
